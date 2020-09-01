4db5465ad237daa8593660e49c670c913f54b818
Steven Buglione <spbug1999@gmail.com>
2020-08-31 23:23:06 -0400

    Inital Commit

---
+++ Regalia-Ordering-System-WebApi/Regalia-Ordering-System-WebApi/WeatherForecast.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace Regalia_Ordering_System_WebApi
+{
+    public class WeatherForecast
+    {
+        public DateTime Date { get; set; }
+
+        public int TemperatureC { get; set; }
+
+        public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+        public string Summary { get; set; }
+    }
+}
