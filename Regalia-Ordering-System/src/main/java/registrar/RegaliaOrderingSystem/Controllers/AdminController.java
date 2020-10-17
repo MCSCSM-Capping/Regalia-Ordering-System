d2d101b4a3f73fe414bd473776248c9b9fee63c6
Steven Buglione <spbug1999@gmail.com>
2020-10-17 16:01:17 -0400

    Added Admin Profile/logout

--- Regalia-Ordering-System/src/main/java/registrar/RegaliaOrderingSystem/Controllers/AdminController.java
+++ Regalia-Ordering-System/src/main/java/registrar/RegaliaOrderingSystem/Controllers/AdminController.java
@@ -2,14 +2,17 @@ package registrar.RegaliaOrderingSystem.Controllers;
 
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import registrar.RegaliaOrderingSystem.Dao.Repository.UserRepository;
 import registrar.RegaliaOrderingSystem.Dao.Service.*;
 import registrar.RegaliaOrderingSystem.Models.User;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 @Controller
@@ -34,11 +37,32 @@ public class AdminController {
     @Autowired
     private StateService _stateService;
 
+    @Autowired
+    private AuthService _authService;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Value("${casLogoutUrl}")
+    private String casLogoutUrl;
+
 
 
     //Admin Dashboard routes
     @RequestMapping("/admin_page_active_users")
-    public String viewAdminActiveUsers(Model model){
+    public String viewAdminActiveUsers(HttpServletRequest request, Model model){
+
+        //Get user CWID from principal
+        String CWID = _authService.getUserCWID(request);
+
+        //Get the user Logged in
+        User admin = userRepository.getUserByUsername(CWID);
+
+        //Provide admin Details to model
+        model.addAttribute("admin", admin);
+
+        //Logout url
+        model.addAttribute("logout", casLogoutUrl);
 
         //TODO add logic for only grabbing active users
         List<User> activeUsers = _userService.listAll("enabled");
@@ -51,7 +75,20 @@ public class AdminController {
     }
 
     @RequestMapping("/admin_page_archived_users")
-    public String viewAdminArchivedUsers(Model model){
+    public String viewAdminArchivedUsers(HttpServletRequest request, Model model){
+
+        //Get user CWID from principal
+        String CWID = _authService.getUserCWID(request);
+
+
+        //Get the User Logged in
+        User admin = userRepository.getUserByUsername(CWID);
+
+        //Provide admin Details to model
+        model.addAttribute("admin", admin);
+
+        //Logout url
+        model.addAttribute("logout", casLogoutUrl);
 
         //TODO add logic for only grabbing active users
         List<User> inactiveUsers = _userService.listAll("disabled");
