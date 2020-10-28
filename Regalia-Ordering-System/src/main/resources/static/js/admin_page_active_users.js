ef078d16c5591c7a8b8c9b2867cbfa02915ba7c8
Steven Buglione <spbug1999@gmail.com>
2020-10-28 13:19:41 -0400

    Api Request Refactored

--- Regalia-Ordering-System/src/main/resources/static/js/admin_page_active_users.js
+++ Regalia-Ordering-System/src/main/resources/static/js/admin_page_active_users.js
@@ -1,36 +1,19 @@
 function setUserCwid(user){
-        //Getting form data and storing it in block scope variables
-        let username = user
-
-        console.log(username);
-
-        //Set the form Data with the users CWID
-        let ModalUsername = document.getElementById("modelCwidValue");
-
-        ModalUsername.attribute = username;
-
-        ModalUsername.textContent = username;
-
+    let username = user
+    let ModalUsername = document.getElementById("modelCwidValue");
+    ModalUsername.attribute = username;
+    ModalUsername.textContent = username;
 }
 
 function postDeleteUser(){
+    const route = 'http://localhost:8080/user/delete/';
+    let id = document.getElementById("modelCwidValue").textContent;
+    let url = route + id ;
 
-
-    const url = 'http://localhost:8080/delete';
-
-    let usersCwid = document.getElementById("modelCwidValue").textContent;
-
-    // post body data
-    const user = {
-        username: usersCwid
-    };
-
-    console.log(user);
 
     // request options
     const options = {
         method: 'POST',
-        body: JSON.stringify(user),
         headers: {
             'Content-Type': 'application/json'
         }
@@ -43,68 +26,48 @@ function postDeleteUser(){
 
 
     window.location.href = "/admin_page_active_users"
-
-
-
-
-
 }
 
 
 
 function getUserByCwid(cwid){
+    const url = 'http://localhost:8080/user/';
+    let usersCwid = cwid;
+    let fetchUrl = url + usersCwid;
 
-        const url = 'http://localhost:8080/user/';
+    let firstName = document.getElementById("firstName");
+    let lastName = document.getElementById("lastName");
+    let email = document.getElementById("email");
+    let phoneNumber = document.getElementById("phoneNumber");
+    let department = document.getElementById("department");
+    let capSize = document.getElementById("capSize");
+    let degree = document.getElementById("degree");
+    let weight = document.getElementById("weight");
+    let heightInches = document.getElementById("heightInches");
+    let grantingInstitution = document.getElementById("grantingInstitution");
+    let institutionState = document.getElementById("institutionState");
+    let institutionCity = document.getElementById("institutionCity");
+    let ceremonyDate = document.getElementById("ceremonyDate");
 
-        let usersCwid = cwid;
+    fetch(fetchUrl)
+        .then(response => response.json())
+        .then(json => {
+            firstName.value = json.first_name;
+            lastName.value = json.last_name;
+            email.value = json.email;
+            phoneNumber.value = json.phone_number;
+            department.value = json.department;
+            capSize.value = json.cap_size;
+            degree.value = json.degree;
+            weight.value = json.weight;
+            heightInches.value = json.height_inches;
+            grantingInstitution.value = json.granting_institution;
+            institutionState.value = json.granting_state;
+            institutionCity.value = json.granting_city;
+            ceremonyDate.value = json.ceremony_date;
+        })
 
-        let fetchUrl = url + usersCwid;
-
-
-
-        let firstName = document.getElementById("firstName");
-        let lastName = document.getElementById("lastName");
-        let email = document.getElementById("email");
-        let phoneNumber = document.getElementById("phoneNumber");
-        let department = document.getElementById("department");
-        let capSize = document.getElementById("capSize");
-        let degree = document.getElementById("degree");
-        let weight = document.getElementById("weight");
-        let heightInches = document.getElementById("heightInches");
-        let grantingInstitution = document.getElementById("grantingInstitution");
-        let institutionState = document.getElementById("institutionState");
-        let institutionCity = document.getElementById("institutionCity");
-        let ceremonyDate = document.getElementById("ceremonyDate");
-
-        fetch(fetchUrl)
-            .then(response => response.json())
-            .then(json => {
-
-
-                firstName.value = json.first_name;
-                lastName.value = json.last_name;
-                email.value = json.email;
-                phoneNumber.value = json.phone_number;
-                department.value = json.department;
-                capSize.value = json.cap_size;
-                degree.value = json.degree;
-                weight.value = json.weight;
-                heightInches.value = json.height_inches;
-                grantingInstitution.value = json.granting_institution;
-                institutionState.value = json.granting_state;
-                institutionCity.value = json.granting_city;
-                ceremonyDate.value = json.ceremony_date;
-
-                console.log(json)
-
-            })
-
-
-
-        firstName.textContent = "Steven"
-
-
-
+    firstName.textContent = "Steven"
 
 }
 
--- Regalia-Ordering-System/src/main/resources/static/js/admin_page_archived_users.js
+++ Regalia-Ordering-System/src/main/resources/static/js/admin_page_archived_users.js
@@ -1,36 +1,19 @@
 function getUserCwid(user){
-        //Getting form data and storing it in block scope variables
+
         let username = user;
-
-        console.log(username);
-
-        //Set the form Data with the users CWID
         let ModalUsername = document.getElementById("modelCwidValue");
-
         ModalUsername.attribute = username;
-
         ModalUsername.textContent = username;
 
 }
 
 function postRestoreUser(){
-
-
-    const url = 'http://localhost:8080/restore';
-
-    let usersCwid = document.getElementById("modelCwidValue").textContent;
-
-    // post body data
-    const user = {
-        username: usersCwid
-    };
-
-    console.log(user);
-
+    const route = 'http://localhost:8080/user/restore/';
+    let id = document.getElementById("modelCwidValue").textContent;
+    let url = route + id;
     // request options
     const options = {
         method: 'POST',
-        body: JSON.stringify(user),
         headers: {
             'Content-Type': 'application/json'
         }
@@ -43,6 +26,4 @@ function postRestoreUser(){
 
 
   window.location.href = "/admin_page_archived_users"
-
-
 }
